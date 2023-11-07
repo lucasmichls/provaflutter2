65a3408c7d109510d19afd716021e378e861fe87
Lucas Michels <lucaasmich@gmail.com>
2023-11-06 21:21:03 -0300

    adicionado projeto da prova

---
+++ lib/main.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MeuAppFinancas());
+}
+
+class MeuAppFinancas extends StatefulWidget {
+  @override
+  _MeuAppFinancasState createState() => _MeuAppFinancasState();
+}
+
+class _MeuAppFinancasState extends State<MeuAppFinancas> {
+  Map<String, dynamic> currencies = {};
+  Map<String, dynamic> bitcoinCurrencies = {};
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCurrencies().then((data) {
+      setState(() {
+        currencies = data;
+      });
+    });
+    fetchBitcoinCurrencies().then((data) {
+      setState(() {
+        bitcoinCurrencies = data;
+      });
+    });
+  }
+
+  Future<Map<String, dynamic>> fetchCurrencies() async {
+    final response = await http.get(Uri.parse(
+        'https://api.hgbrasil.com/finance?key=0c42162b&format=json-cors'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return {
+        'Dólar': {
+          'valor': data['results']['currencies']['USD']['buy'],
+          'variação': data['results']['currencies']['USD']['variation'],
+        },
+        'Euro': {
+          'valor': data['results']['currencies']['EUR']['buy'],
+          'variação': data['results']['currencies']['EUR']['variation'],
+        },
+        'Peso': {
+          'valor': data['results']['currencies']['ARS']['buy'],
+          'variação': data['results']['currencies']['ARS']['variation'],
+        },
+        'Yen': {
+          'valor': data['results']['currencies']['JPY']['buy'],
+          'variação': data['results']['currencies']['JPY']['variation'],
+        },
+      };
+    } else {
+      throw Exception('Failed to load currencies');
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchBitcoinCurrencies() async {
+    final response = await http.get(Uri.parse(
+        'https://api.hgbrasil.com/finance?key=0c42162b&format=json-cors'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return {
+        'Blockchain': {
+          'valor': data['results']['currencies']['BTC']['buy'],
+          'variação': data['results']['currencies']['BTC']['variation'],
+        },
+        'Coinbase': {
+          'valor': data['results']['currencies']['BTC']['buy'],
+          'variação': data['results']['currencies']['BTC']['variation'],
+        },
+        'BitStamp': {
+          'valor': data['results']['currencies']['BTC']['buy'],
+          'variação': data['results']['currencies']['BTC']['variation'],
+        },
+        'FoxBit': {
+          'valor': data['results']['currencies']['BTC']['buy'],
+          'variação': data['results']['currencies']['BTC']['variation'],
+        },
+      };
+    } else {
+      throw Exception('Failed to load Bitcoin currencies');
+    }
+  }
+
+  int _currentIndex = 0;
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Finanças de Hoje'),
+          backgroundColor: Colors.green[900],
+        ),
+        body: _buildPage(_currentIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.green[900],
+          onTap: _onTabTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money),
+              label: 'Moedas',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.show_chart),
+              label: 'Ações',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.monetization_on),
+              label: 'BitCoin',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPage(int index) {
+    switch (index) {
+      case 0:
+        return MoedasPage(currencies);
+      case 1:
+        return AcoesPage();
+      case 2:
+        return BitCoinPage(bitcoinCurrencies);
+      default:
+        return Container();
+    }
+  }
+}
+
+class MoedasPage extends StatelessWidget {
+  final Map<String, dynamic> currencies;
+
+  MoedasPage(this.currencies);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Moedas'),
+        backgroundColor: Colors.green[900],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            _buildCurrency("Dólar", currencies['Dólar']),
+            _buildCurrency("Euro", currencies['Euro']),
+            _buildCurrency("Peso", currencies['Peso']),
+            _buildCurrency("Yen", currencies['Yen']),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green[900],
+      ),
+    );
+  }
+
+  Widget _buildCurrency(String name, Map<String, dynamic> data) {
+    double value = data['valor'];
+    double variation = data['variação'];
+
+    Color textColor = variation >= 0 ? Colors.blue : Colors.red;
+    Color backgroundColor = variation >= 0 ? Colors.blue : Colors.red;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$name: ${value.toStringAsFixed(4)}',
+            style: TextStyle(fontSize: 18, color: textColor),
+          ),
+          SizedBox(width: 8),
+          Container(
+            padding: EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text(
+              'Variação: ${variation.toStringAsFixed(4)}',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AcoesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ações'),
+        backgroundColor: Colors.green[900],
+      ),
+      body: Center(
+        child: Text('Conteúdo da página de Ações'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green[900],
+      ),
+    );
+  }
+}
+
+class BitCoinPage extends StatelessWidget {
+  final Map<String, dynamic> bitcoinCurrencies;
+
+  BitCoinPage(this.bitcoinCurrencies);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BitCoin'),
+        backgroundColor: Colors.green[900],
+      ),
+      body: Center(
+        child: Column(
+          children: bitcoinCurrencies.entries.map((entry) {
+            String name = entry.key;
+            double value = entry.value['valor'];
+            double variation = entry.value['variação'];
+
+            Color textColor = variation >= 0 ? Colors.blue : Colors.red;
+            Color backgroundColor = variation >= 0 ? Colors.blue : Colors.red;
+
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '$name: ${value.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 18, color: textColor),
+                  ),
+                  SizedBox(width: 8),
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: backgroundColor,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text(
+                      'Variação: ${variation.toStringAsFixed(3)}',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green[900],
+      ),
+    );
+  }
+}
